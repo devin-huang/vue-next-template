d84390cb1617c308c6c68c6646340b9fd391aeac
huang dehui <342514492@qq.com>
2020-09-10 15:20:33 +0800

    update pageage.json

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -17,6 +17,7 @@ export default defineComponent({
     msg: String,
   },
   setup(props, vue) {
+    const aa = 'aaaa444412'
     console.log(props, vue)
     const count = ref(0)
     const add = () => {
